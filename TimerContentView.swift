02627fe5d64c1b65f6cb8a1c3651676ea3beeead
Anirudh <anirudh.ms@postman.com>
2025-12-07 18:35:19 +0530

    Floating resizable panel

---
+++ TimerContentView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerContentView.swift
+//  Overlay Timer 2
+//
+//  Created by Anirudh Kumar on 07/12/25.
+//
+
+import SwiftUI
+
+struct TimerContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerContentView()
+}
